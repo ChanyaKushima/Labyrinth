4afc9b95045ddf56538f79cec439ee177a49adfe
ChanyaKushima <49159503+ChanyaKushima@users.noreply.github.com>
2019-07-16 10:43:15 +0900

    Add files via upload

--- Labyrinth/WinForms1/Form1.cs
+++ Labyrinth/WinForms1/Form1.cs
@@ -14,34 +14,47 @@ using static System.Windows.Input.Keyboard;
 
 namespace WinForms1
 {
-using System.Windows.Forms;
+
+    using System.Windows.Forms;
     public partial class Form1 : Form
     {
         private enum Direction
         {
-            None,
             Up,
             Down,
             Right,
             Left,
         }
 
-        static readonly IReadOnlyCollection<Direction> EveryDirections =
-                new[] { Direction.Down, Direction.Left, Direction.Right, Direction.Up };
+        static readonly IReadOnlyList<Direction> EveryDirections =
+                new [] { Direction.Down, Direction.Left, Direction.Right, Direction.Up };
+        static readonly StringFormat FormatCenter = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+        static readonly Brush BackScreen = new SolidBrush(Color.FromArgb(50, Color.Black));
+        static readonly PointF CenterPosition = new PointF(MapDisplayWidth / 2.0f, MapDisplayHeight / 2.0f);
+        static readonly string DefaultTitle = "Labyrinth";
+        static readonly Size MapDisplaySize = new Size(MapDisplayWidth, MapDisplayHeight);
+        static readonly Rectangle MapDisplayRect = new Rectangle(default, MapDisplaySize);
 
-        const int PieceSize = 10;
-        const int MapWidth = 157;
-        const int MapHeight = 95;
+        const int PieceSize = 5;
+        const int MapWidth = 361;
+        const int MapHeight = 201;
+        const int MapDisplayWidth = MapWidth * PieceSize;
+        const int MapDisplayHeight = MapHeight * PieceSize;
 
         const int GoalX = MapWidth - 2;
         const int GoalY = MapHeight - 2;
+        Brush wallBrush = Brushes.DeepSkyBlue;
+        Brush roadBrush = Brushes.AliceBlue;
+        Brush myBrush = Brushes.SkyBlue;
+        Brush goalPointBrush = Brushes.Gold;
 
+        private Image MapImage;
         private Piece[,] Map = new Piece[MapWidth, MapHeight];
 
         private Random rand = new Random();
         private Timer timer = new Timer()
         {
-            Enabled = true,
+            Enabled = false,
             Interval = 50,
         };
 
@@ -50,55 +63,48 @@ using System.Windows.Forms;
 
         bool GameClear = false;
 
+        private static List<(int X, int Y)> GeneralPurposeList = new List<(int X, int Y)>();
+
         public Form1()
         {
             InitializeComponent();
-            this.StartPosition = FormStartPosition.CenterScreen;
-            ClientSize = new Size(MapWidth * PieceSize, MapHeight * PieceSize);
+            StartPosition = FormStartPosition.CenterScreen;
+            ClientSize = new Size(MapDisplayWidth, MapDisplayHeight);
             DoubleBuffered = true;
             timer.Tick += MoveMeByKey_OnTick;
 
-            DigLabyrinth(MapWidth, MapHeight, Map);
+            DigLabyrinth(Map, true);
+            MapImage = GetMapImage(Map, roadBrush, wallBrush);
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
-            Graphics g = e.Graphics;
-            Brush wallBrush = Brushes.DeepSkyBlue;
-            Brush roadBrush = Brushes.AliceBlue;
-            Brush myBrush = Brushes.SkyBlue;
-            Brush goalBrush = Brushes.Gold;
-            const int mySize = (int)(PieceSize*0.8);
+            const int mySize = ((int)(PieceSize * 0.8) > 1) ? (int)(PieceSize * 0.8) : 1;
             const int goalSize = mySize;
             const float myMargin = (PieceSize - mySize) / 2.0f;
             const float goalMargin = (PieceSize - goalSize) / 2.0f;
+            Graphics g = e.Graphics;
 
+            DateTime start = DateTime.Now;
 
-            for (int x = 0; x < MapWidth; x++)
-            {
-                for (int y = 0; y < MapHeight; y++)
-                {
-                    Brush brush = Map[x, y].IsWall ? wallBrush : roadBrush;
-                    g.FillRectangle(brush, x * PieceSize, y * PieceSize, PieceSize, PieceSize);
-                }
-            }
+            g.DrawImage(MapImage, new Point());
 
             g.FillEllipse(myBrush, m_x * PieceSize + myMargin, m_y * PieceSize + myMargin, mySize, mySize);
-            g.FillEllipse(goalBrush, GoalX * PieceSize + goalMargin, GoalY * PieceSize + goalMargin, goalSize, goalSize);
+            g.FillEllipse(goalPointBrush, GoalX * PieceSize + goalMargin, GoalY * PieceSize + goalMargin, goalSize, goalSize);
 
             if (GameClear)
             {
                 var fontSize = Math.Min(ClientSize.Width / 4, ClientSize.Height / 2);
-                var format = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
-                var center = new PointF(ClientSize.Width / 2.0f, ClientSize.Height / 2.0f);
                 var font = new Font(FontFamily.GenericSerif, fontSize);
-                var backScreen = new SolidBrush(Color.FromArgb(50, Color.Black));
 
-                g.FillRectangle(backScreen, new Rectangle(new Point(), ClientSize));
-                g.DrawString("Goal!", font, Brushes.Red, center, format);
+                g.FillRectangle(BackScreen, MapDisplayRect);
+                g.DrawString("Goal!", font, Brushes.Red, CenterPosition, FormatCenter);
             }
+            DateTime end = DateTime.Now;
+
+            Text = DefaultTitle + " - " + (end - start).TotalMilliseconds.ToString();
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
@@ -112,7 +118,13 @@ using System.Windows.Forms;
                 GameLoad();
                 Refresh();
             }
+
+            if (key == Keys.Up || key == Keys.Down || key == Keys.Left || key == Keys.Right)
+            {
+                timer.Start();
+            }
         }
+
 
         private void MoveMeByKey_OnTick(object sender, EventArgs e)
         {
@@ -134,10 +146,16 @@ using System.Windows.Forms;
                 {
                     MoveMe(-1, 0);
                 }
+                else
+                {
+                    timer.Stop();
+                    return;
+                }
             }
-            if ((m_x, m_y) == (GoalX, GoalY))
+            if (m_x == GoalX && m_y == GoalY)
             {
                 GameClear = true;
+                timer.Stop();
             }
             Refresh();
         }
@@ -159,26 +177,21 @@ using System.Windows.Forms;
             }
         }
 
-        private static void DigLabyrinth(int width, int heigth, Piece[,] map)
+        private static void DigLabyrinth(Piece[,] map, bool mapInited)
         {
-            if (map is null)
-            {
-                throw new ArgumentNullException(nameof(map));
-            }
-            if (width % 2 == 0 || heigth % 2 == 0 || width < 5 || heigth < 5)
-            {
-                throw new ArgumentException("引数は5以上の奇数を入力してください");
-            }
-            if (map.GetLength(0) < width || map.GetLength(1) < heigth)
-            {
-                throw new ArgumentException("Mapが生成されていません");
-            }
+            int width = map.GetLength(0);
+            int height = map.GetLength(1);
 
-            for (int x = 0; x < width; x++)
+            ThrowHelper(width, height, map);
+
+            if (!mapInited)
             {
-                for (int y = 0; y < heigth; y++)
+                for (int x = 0; x < width; x++)
                 {
-                    map[x, y] = new Piece();
+                    for (int y = 0; y < height; y++)
+                    {
+                        map[x, y] = default;
+                    }
                 }
             }
 
@@ -190,7 +203,7 @@ using System.Windows.Forms;
             int nowX = startX;
             int nowY = startY;
 
-            List<(int X, int Y)> digablePoints = new List<(int X, int Y)>();
+            List<(int X, int Y)> digablePoints = null;
 
             int count = 0;
 
@@ -200,17 +213,16 @@ using System.Windows.Forms;
             {
                 Dig1Line(map, ref nowX, ref nowY);
 
-                for (int x = 1; x < width; x += 2)
-                {
-                    for (int y = 1; y < heigth; y += 2)
-                    {
-                        if (CanDigSomeDirection(x, y, map))
-                        {
-                            digablePoints.Add((x, y));
-                        }
-                    }
-                }
+                var preDigablePoints = digablePoints ?? new List<(int X, int Y)>();
+                var tmpResult = preDigablePoints.
+                    Where((point) => CanDigSomeDirection(point.X, point.Y, map));
 
+                digablePoints = tmpResult.ToList();
+
+                if (digablePoints.Count == 0)
+                {
+                    digablePoints = GetDigablePoints(width, height, map);
+                }
 
                 count = digablePoints.Count;
 
@@ -220,9 +232,55 @@ using System.Windows.Forms;
                 }
 
                 (nowX, nowY) = digablePoints[rand.Next(count)];
-
-                digablePoints.Clear();
             }
+        }
+
+        private static Image GetMapImage(Piece[,] map, Brush roadBrush, Brush wallBrush)
+        {
+            Image mapImage = new Bitmap(MapDisplayWidth, MapDisplayHeight);
+            var g = Graphics.FromImage(mapImage);
+
+            for (int x = 0; x < MapWidth; x++)
+            {
+                for (int y = 0; y < MapHeight; y++)
+                {
+                    Brush brush = map[x, y].IsRoad ? roadBrush : wallBrush;
+                    g.FillRectangle(brush, x * PieceSize, y * PieceSize, PieceSize, PieceSize);
+                }
+            }
+
+            return mapImage;
+        }
+
+        private static void ThrowHelper(int width, int heigth, Piece[,] map)
+        {
+            if (map is null)
+            {
+                throw new ArgumentNullException(nameof(map));
+            }
+            if (width % 2 == 0 || heigth % 2 == 0 || width < 5 || heigth < 5)
+            {
+                throw new ArgumentException("引数は5以上の奇数を入力してください");
+            }
+        }
+
+        private static List<(int X, int Y)> GetDigablePoints(int width, int heigth, Piece[,] map)
+        {
+            GeneralPurposeList.Clear();
+            var digablePoints = GeneralPurposeList;
+
+            for (int x = 1; x < width; x += 2)
+            {
+                for (int y = 1; y < heigth; y += 2)
+                {
+                    if (CanDigSomeDirection(x, y, map))
+                    {
+                        digablePoints.Add((x, y));
+                    }
+                }
+            }
+
+            return digablePoints;
         }
 
         private static void Dig1Line(Piece[,] map, ref int nowX, ref int nowY)
@@ -249,12 +307,17 @@ using System.Windows.Forms;
 
                 Dig2Piece(ref nowX, ref nowY, map, movableDirections[rand.Next(count)]);
 
+                if (nowX == GoalX && nowY == GoalY)
+                {
+                    break;
+                }
+
                 movableDirections.Clear();
             }
-
         }
 
-        private static void Dig2Piece(ref int nowX, ref int nowY,Piece[,] map, Direction direction)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void Dig2Piece(ref int nowX, ref int nowY, Piece[,] map, Direction direction)
         {
             switch (direction)
             {
@@ -281,37 +344,21 @@ using System.Windows.Forms;
             }
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static bool CanDig2Piece(int nowX, int nowY,Piece[,] map, Direction direction)
+        private static bool CanDig2Piece(int nowX, int nowY, Piece[,] map, Direction direction)
         {
-            try
+            switch (direction)
             {
-                switch (direction)
-                {
-                    case Direction.Up:
-                        return map[nowX, nowY - 0].IsRoad &&
-                               map[nowX, nowY - 1].IsWall &&
-                               map[nowX, nowY - 2].IsWall &&
-                               map[nowX, nowY - 3].IsWall;
-                    case Direction.Down:
-                        return map[nowX, nowY + 0].IsRoad &&
-                               map[nowX, nowY + 1].IsWall &&
-                               map[nowX, nowY + 2].IsWall &&
-                               map[nowX, nowY + 3].IsWall;
-                    case Direction.Right:
-                        return map[nowX + 0, nowY].IsRoad &&
-                               map[nowX + 1, nowY].IsWall &&
-                               map[nowX + 2, nowY].IsWall &&
-                               map[nowX + 3, nowY].IsWall;
-                    case Direction.Left:
-                        return map[nowX - 0, nowY].IsRoad &&
-                               map[nowX - 1, nowY].IsWall &&
-                               map[nowX - 2, nowY].IsWall &&
-                               map[nowX - 3, nowY].IsWall;
-                }
+                case Direction.Up:
+                    return nowY > 2 && map[nowX, nowY - 0].IsRoad && map[nowX, nowY - 2].IsWall;
+                case Direction.Left:
+                    return nowX > 2 && map[nowX - 0, nowY].IsRoad && map[nowX - 2, nowY].IsWall;
+                case Direction.Down:
+                    return nowY < map.GetLength(1) - 3 && map[nowX, nowY + 0].IsRoad && map[nowX, nowY + 2].IsWall;
+                case Direction.Right:
+                    return nowX < map.GetLength(0) - 3 && map[nowX + 0, nowY].IsRoad && map[nowX + 2, nowY].IsWall;
+                default:
+                    return false;
             }
-            catch (Exception) { }
-
-            return false;
         }
         private static bool CanDigSomeDirection(int nowX, int nowY, Piece[,] map)
         {
@@ -319,9 +366,12 @@ using System.Windows.Forms;
                 CanDig2Piece(nowX, nowY, map, Direction.Left) || CanDig2Piece(nowX, nowY, map, Direction.Right);
         }
 
+
+
         private void GameLoad()
         {
-            DigLabyrinth(MapWidth, MapHeight, Map);
+            DigLabyrinth(Map, false);
+            MapImage=GetMapImage(Map, roadBrush, wallBrush);
             (m_x, m_y) = (1, 1);
             GameClear = false;
         }
